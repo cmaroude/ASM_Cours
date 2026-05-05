7624456ac97e73c08f2e0ecad3d86e587d031709
Chloe Maroude <cmaroude@c2r11p3.42nice.fr>
2026-05-05 12:06:44 +0200

    whitespace uniformity with other files

--- libasm/ft_strcmp.s
+++ libasm/ft_strcmp.s
@@ -4,22 +4,22 @@ section .text
 	global ft_strcmp
 
 ft_strcmp:
-    xor rcx, rcx ; init rcx 0
+	xor rcx, rcx ; init rcx 0
 
 .loop:
-    movzx eax, byte [rdi +rcx] ; passage de 8 a 32 bit
-    movzx ebx, byte [rsi + rcx]
-    cmp eax, ebx
-    jne .diff
-    test al, al
-    je .equal
-    inc rcx
-    jmp .loop
+	movzx eax, byte [rdi +rcx] ; passage de 8 a 32 bit
+	movzx ebx, byte [rsi + rcx]
+	cmp eax, ebx
+	jne .diff
+	test al, al
+	je .equal
+	inc rcx
+	jmp .loop
 
 .diff:
-    sub eax, ebx
-    ret
+	sub eax, ebx
+	ret
 
 .equal:
-    xor eax, eax
-    ret
+	xor eax, eax
+	ret
