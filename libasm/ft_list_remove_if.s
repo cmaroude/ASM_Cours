8098357484d9bd5f28219d640352c555f8c1e859
Chloe Maroude <cmaroude@c2r11p3.42nice.fr>
2026-05-05 12:09:13 +0200

    Error correction for call functions and stack alignment

--- libasm/ft_list_remove_if.s
+++ libasm/ft_list_remove_if.s
@@ -6,46 +6,55 @@ section .text
 	global ft_list_remove_if
 
 ft_list_remove_if:
-    ; RDI = t_list **begin_list
-    ; RSI = data_ref
-    ; RDX = cmp
-    ; RCX = free_fct
-    push rbx
-    push r12
-    push r13
-    push r14
-    mov r12, rdi        ; r12 = begin_list (t_list **)
+	; RDI = t_list **begin_list
+	; RSI = data_ref
+	; RDX = cmp
+	; RCX = free_fct
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	mov r12, rdi  ; r12 = begin_list (t_list **)
+	mov r13, rsi  ; r13 = data_ref
+	mov r14, rdx  ; r14 = cmp
+	mov r15, rcx	; r15 = free_fct
 
 .loop:
-    mov rbx, [r12]      ; rbx = *begin_list
-    test rbx, rbx
-    je .end
-    ; cmp((*begin_list)->data, data_ref)
-    mov rdi, [rbx]      ; arg1 = node->data
-    mov rsi, rsi        ; arg2 = data_ref
-    call rdx            ; cmp()
-    test eax, eax
-    jne .no_remove
-    mov r13, [r12]      ; tmp = *begin_list
-    mov r14, [r13 + 8]  ; tmp->next
-    mov [r12], r14      ; *begin_list = tmp->next
-    ; free_fct(tmp->data)
-    mov rdi, [r13]
-    call rcx
-    ; free(tmp)
-    mov rdi, r13
-    call free
-    jmp .loop
+	mov rbx, [r12]	; rbx = *begin_list
+	test rbx, rbx
+	je .end
+	; cmp((*begin_list)->data, data_ref)
+	mov rdi, [rbx]	; arg1 = node->data
+	mov rsi, r13	; arg2 = data_ref
+	sub rsp, 8
+	call r14	; cmp()
+	add rsp, 8
+	test eax, eax
+	jne .no_remove
+	mov rdx, [rbx + 8]	; next
+	mov [r12], rdx      ; *begin_list = tmp->next
+	; free_fct(tmp->data)
+	mov rdi, [rbx]
+	sub rsp, 8
+	call r15
+	add rsp, 8
+	; free(tmp)
+	mov rdi, rbx
+	sub rsp, 8
+	call free wrt ..plt
+	add rsp, 8
+	jmp .loop
 
 .no_remove:
-    ; begin_list = &(*begin_list)->next
-    mov r12, rbx
-    add r12, 8          ; &node->next
-    jmp .loop
+	; begin_list = &(*begin_list)->next
+	lea r12, [rbx + 8]
+	jmp .loop
 
 .end:
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    ret
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	ret
--- libasm/ft_list_size.s
+++ libasm/ft_list_size.s
@@ -4,14 +4,14 @@ section .text
 	global ft_list_size
 
 ft_list_size:
-    xor rax, rax ; init compteur 0
+	xor rax, rax ; init compteur 0
 
 .loop:
-    test rdi, rdi ; si lst == NULL
-    jz .done
-    inc rax ; i++
-    mov rdi, [rdi + 8] ; lst = lst->next
-    jmp .loop
+	test rdi, rdi ; si lst == NULL
+	jz .done
+	inc rax ; i++
+	mov rdi, [rdi + 8] ; lst = lst->next
+	jmp .loop
 
 .done:
-    ret
+	ret
